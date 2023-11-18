a7ced81f4b49ff5ae7d7bf73088fcc7bba87d1f9
Temesgen Zewude <temesgenzewude21@gmail.com>
2023-11-18 19:14:04 +0300

    add the other fields and location access and make it scrollable

--- ultimate-app/lib/authentications/sign_up/widget/sign_up_tab.dart
+++ ultimate-app/lib/authentications/sign_up/widget/sign_up_tab.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_ultimate/common/bloc/auth/authentication_bloc.dart';
 import 'package:flutter_ultimate/common/route/routes.dart';
 import 'package:flutter_ultimate/data/models/authentication_model.dart';
+import 'package:geolocator/geolocator.dart';
 
 import '../../../app/widget_support.dart';
 import '../../../common/constant/colors.dart';
@@ -25,39 +26,117 @@ class _SignUpTabState extends State<SignUpTab> {
   TextEditingController passwordCtl = TextEditingController();
   FocusNode passwordFn = FocusNode();
   TextEditingController repasswordCtl = TextEditingController();
+  TextEditingController nameCtl = TextEditingController();
+  FocusNode nameFn = FocusNode();
+  TextEditingController phoneCtl = TextEditingController();
+  FocusNode phoneFn = FocusNode();
+  TextEditingController addressCtl = TextEditingController();
+  FocusNode addressFn = FocusNode();
   FocusNode repasswordFn = FocusNode();
   bool showPass = false;
   bool showRePass = false;
+  Position? _currentPosition;
+
+  Future<bool> _handleLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location services are disabled. Please enable the services')));
+      return false;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Location permissions are denied')));
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location permissions are permanently denied, we cannot request permissions.')));
+      return false;
+    }
+    return true;
+  }
+
+  Future<void> _getCurrentPosition() async {
+    final hasPermission = await _handleLocationPermission();
+
+    if (!hasPermission) return;
+    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
+        .then((Position position) {
+      setState(() => _currentPosition = position);
+      debugPrint('location: ${_currentPosition?.latitude}');
+      debugPrint('location: ${_currentPosition?.longitude}');
+    }).catchError((dynamic e) {
+      debugPrint(e);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentPosition();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          GradientText(
-            'Welcom to  Ultimate!',
-            style: const TextStyle(
-                fontSize: 32,
-                height: 1,
-                fontWeight: FontWeight.w700,
-                fontFamily: 'SpaceGrotesk'),
-            gradient: LinearGradient(colors: [
-              const Color(0xFFCFE1FD).withOpacity(0.9),
-              const Color(0xFFFFFDE1).withOpacity(0.9),
-            ]),
-          ),
-          Column(
-            children: [
-              TextFieldCpn(
-                controller: usernameCtl,
-                focusNode: usernameFn,
-                labelText: 'Username',
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                child: TextFieldPassCpn(
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            GradientText(
+              'Welcom to  Ultimate!',
+              style: const TextStyle(
+                  fontSize: 32,
+                  height: 1,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: 'SpaceGrotesk'),
+              gradient: LinearGradient(colors: [
+                const Color(0xFFCFE1FD).withOpacity(0.9),
+                const Color(0xFFFFFDE1).withOpacity(0.9),
+              ]),
+            ),
+            Column(
+              children: [
+                TextFieldCpn(
+                  controller: nameCtl,
+                  focusNode: nameFn,
+                  labelText: 'Name',
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: TextFieldCpn(
+                    controller: usernameCtl,
+                    focusNode: usernameFn,
+                    labelText: 'Email',
+                  ),
+                ),
+                TextFieldCpn(
+                  controller: addressCtl,
+                  focusNode: addressFn,
+                  labelText: 'Address',
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: TextFieldCpn(
+                    controller: phoneCtl,
+                    focusNode: phoneFn,
+                    labelText: 'Phone',
+                    keyboardType: TextInputType.phone,
+                  ),
+                ),
+                TextFieldPassCpn(
                     controller: passwordCtl,
                     focusNode: passwordFn,
                     labelText: 'Password',
@@ -70,89 +149,89 @@ class _SignUpTabState extends State<SignUpTab> {
                         showPass = !showPass;
                       });
                     }),
-              ),
-              TextFieldPassCpn(
-                  controller: repasswordCtl,
-                  focusNode: repasswordFn,
-                  labelText: 'Re Password',
-                  showSuffixIcon: true,
-                  obscureText: !showRePass,
-                  suffixIcon: showRePass ? eye : eyeSlash,
-                  colorSuffixIcon: grey500,
-                  functionSuffer: () {
-                    setState(() {
-                      showRePass = !showRePass;
-                    });
-                  }),
-              const SizedBox(height: 32),
-              AppWidget.typeButtonStartAction(
-                  context: context,
-                  input: 'Sign Up Now',
-                  onPressed: () {
-                    final AuthenticationModel user = AuthenticationModel(
-                      userName: usernameCtl.text,
-                      password: passwordCtl.text,
-                    );
-                    // Dispatch SignUpEvent to Authentication Bloc with AuthenticationModel
-                    BlocProvider.of<AuthenticationBloc>(context).add(
-                      SignUpEvent(newUser: user),
-                    );
-                    Navigator.of(context).pushNamed(Routes.signUp);
-                  },
-                  colorAsset: grey1100,
-                  icon: icKeyboardRight,
-                  sizeAsset: 24,
-                  bgColor: primary,
-                  borderColor: primary,
-                  textColor: grey1100),
-            ],
-          ),
-          Align(
-            alignment: Alignment.center,
-            child: Text(
-              'or Sign Up with social account',
-              style: subhead(color: grey600),
+                TextFieldPassCpn(
+                    controller: repasswordCtl,
+                    focusNode: repasswordFn,
+                    labelText: 'Re Password',
+                    showSuffixIcon: true,
+                    obscureText: !showRePass,
+                    suffixIcon: showRePass ? eye : eyeSlash,
+                    colorSuffixIcon: grey500,
+                    functionSuffer: () {
+                      setState(() {
+                        showRePass = !showRePass;
+                      });
+                    }),
+                const SizedBox(height: 32),
+                AppWidget.typeButtonStartAction(
+                    context: context,
+                    input: 'Sign Up Now',
+                    onPressed: () {
+                      final AuthenticationModel user = AuthenticationModel(
+                        userName: usernameCtl.text,
+                        password: passwordCtl.text,
+                      );
+                      // Dispatch SignUpEvent to Authentication Bloc with AuthenticationModel
+                      BlocProvider.of<AuthenticationBloc>(context).add(
+                        SignUpEvent(newUser: user),
+                      );
+                      Navigator.of(context).pushNamed(Routes.signUp);
+                    },
+                    colorAsset: grey1100,
+                    icon: icKeyboardRight,
+                    sizeAsset: 24,
+                    bgColor: primary,
+                    borderColor: primary,
+                    textColor: grey1100),
+              ],
             ),
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: AppWidget.typeButtonStartAction2(
-                    context: context,
-                    input: 'Facebook',
-                    onPressed: () {},
-                    icon: icFacebook,
-                    sizeAsset: 24,
-                    bgColor: grey200,
-                    borderColor: grey200,
-                    textColor: grey1100),
-              ),
-              const SizedBox(width: 16),
-              Expanded(
-                child: AppWidget.typeButtonStartAction2(
-                    context: context,
-                    input: 'Twitter',
-                    onPressed: () {},
-                    icon: icTwitter,
-                    sizeAsset: 24,
-                    bgColor: grey200,
-                    borderColor: grey200,
-                    textColor: grey1100),
-              ),
-            ],
-          ),
-          Align(
-            alignment: Alignment.center,
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 8, left: 16, right: 16),
+            Align(
+              alignment: Alignment.center,
               child: Text(
-                'By clicking Sign Up you are agreeing to the Terms of Use and the Privacy Policy',
-                textAlign: TextAlign.center,
+                'or Sign Up with social account',
                 style: subhead(color: grey600),
               ),
             ),
-          ),
-        ],
+            Row(
+              children: [
+                Expanded(
+                  child: AppWidget.typeButtonStartAction2(
+                      context: context,
+                      input: 'Facebook',
+                      onPressed: () {},
+                      icon: icFacebook,
+                      sizeAsset: 24,
+                      bgColor: grey200,
+                      borderColor: grey200,
+                      textColor: grey1100),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: AppWidget.typeButtonStartAction2(
+                      context: context,
+                      input: 'Twitter',
+                      onPressed: () {},
+                      icon: icTwitter,
+                      sizeAsset: 24,
+                      bgColor: grey200,
+                      borderColor: grey200,
+                      textColor: grey1100),
+                ),
+              ],
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 8, left: 16, right: 16),
+                child: Text(
+                  'By clicking Sign Up you are agreeing to the Terms of Use and the Privacy Policy',
+                  textAlign: TextAlign.center,
+                  style: subhead(color: grey600),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
