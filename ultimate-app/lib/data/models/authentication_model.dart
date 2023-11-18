522e2bc4e69c0ed4b0378f0de1542d136ae82bb4
Stargazing-11 <samuelkifle199314@gmail.com>
2023-11-18 14:13:15 +0300

    update on model

--- ultimate-app/lib/data/models/authentication_model.dart
+++ ultimate-app/lib/data/models/authentication_model.dart
@@ -1,8 +1,27 @@
+// ignore_for_file: sort_constructors_first
+
 class AuthenticationModel {
   String userName;
   String password;
+  String? email;
+  String? phoneNumber;
+  String? birthday;
+  String? address;
+  String? coordinates;
+  String? userType;
+  String? terms;
 
-  AuthenticationModel({required this.userName, required this.password});
+  AuthenticationModel({
+    required this.userName,
+    required this.password,
+    this.email,
+    this.address,
+    this.birthday,
+    this.coordinates,
+    this.phoneNumber,
+    this.terms,
+    this.userType,
+  });
 
   factory AuthenticationModel.fromJson(Map<String, dynamic> json) {
     return AuthenticationModel(
@@ -11,6 +30,8 @@ class AuthenticationModel {
     );
   }
 
-  
-
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'id': userName,
+        'password': password,
+      };
 }
