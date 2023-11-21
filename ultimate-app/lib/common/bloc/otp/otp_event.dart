2f4570bb09581ddc4a6af4614c7fe64220965fc4
Temesgen Zewude <temesgenzewude21@gmail.com>
2023-11-21 23:08:43 +0300

    add user a send and verify otp

--- ultimate-app/lib/common/bloc/otp/otp_event.dart
+++ ultimate-app/lib/common/bloc/otp/otp_event.dart
@@ -10,9 +10,18 @@ abstract class OtpEvent extends Equatable {
 class OtpSent extends OtpEvent {}
 
 class OtpVerified extends OtpEvent {
+  const OtpVerified(this.otp);
   final String otp;
 
-  const OtpVerified(this.otp);
+  @override
+  List<Object> get props => [otp];
+}
+
+class OTPSendUserA extends OtpEvent {}
+
+class OTPVerifyUserA extends OtpEvent {
+  const OTPVerifyUserA(this.otp);
+  final String otp;
 
   @override
   List<Object> get props => [otp];
