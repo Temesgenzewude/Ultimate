ae301fc42ba0c17e51d8a3c1d16756e47a8d8c76
EyasuTesfu <jt272525@gmail.com>
2023-11-24 15:26:30 +0300

    Added lastViewedPage

--- ultimate-app/lib/sharedPreferences.dart
+++ ultimate-app/lib/sharedPreferences.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_ultimate/common/route/routes.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class PrefManager {
@@ -6,12 +7,16 @@ class PrefManager {
   String kUserID = 'userID';
   String kLatitude = 'latitude';
   String kLongitude = 'longitude';
-  String kuserType = 'A';
+  String kUserType = 'A';
+  String kLastViewedPage = Routes.onBoarding1;
   SharedPreferences preferences;
   set token(String? value) => preferences.setString(kToken, value ?? '');
   String? get token => preferences.getString(kToken);
-  set userType(String? value) => preferences.setString(kuserType, value ?? '');
-  String? get userType => preferences.getString(kuserType);
+  set lastViewedPage(String? value) =>
+      preferences.setString(kLastViewedPage, value ?? '');
+  String? get lastViewedPage => preferences.getString(kLastViewedPage);
+  set userType(String? value) => preferences.setString(kUserType, value ?? '');
+  String? get userType => preferences.getString(kUserType);
   set userId(String? value) => preferences.setString(kUserID, value ?? '');
   String? get userID => preferences.getString(kUserID);
   set latitude(String? value) => preferences.setString(kLatitude, value ?? '');
