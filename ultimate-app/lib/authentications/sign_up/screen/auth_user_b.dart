4fc72866463b4cbc8be00cf78387208194367029
Temesgen Zewude <temesgenzewude21@gmail.com>
2023-11-27 23:12:30 +0300

    prevent back navigation from auth user b

--- ultimate-app/lib/authentications/sign_up/screen/auth_user_b.dart
+++ ultimate-app/lib/authentications/sign_up/screen/auth_user_b.dart
@@ -30,72 +30,78 @@ class _AuthPageBState extends State<AuthPageB>
   Widget build(BuildContext context) {
     final height = AppWidget.getHeightScreen(context);
     final width = AppWidget.getWidthScreen(context);
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.asset(
-                bg10,
-                width: width,
-                height: height / 3.5,
-                fit: BoxFit.fill,
-              ),
-              Container(
-                width: width,
-                height: height / 1.25,
-                decoration: const BoxDecoration(
-                    color: grey100,
-                    borderRadius:
-                        BorderRadius.vertical(top: Radius.circular(24))),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                      margin:
-                          const EdgeInsets.only(top: 16, left: 16, right: 16),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16),
-                          color: grey200,
-                          border: Border.all(color: grey300)),
-                      child: TabBar(
-                        onTap: (index) {
-                          setState(() {
-                            _currentIndex = index;
-                          });
-                        },
-                        controller: _controller,
-                        indicator: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16),
-                          color: primary,
-                        ),
-                        labelStyle: footnote(color: grey1100),
-                        unselectedLabelStyle: footnote(color: grey1100),
-                        labelColor: grey1100,
-                        unselectedLabelColor: grey1100,
-                        indicatorColor: primary,
-                        indicatorPadding: const EdgeInsets.all(2),
-                        labelPadding: const EdgeInsets.symmetric(horizontal: 0),
-                        tabs: [
-                          Tab(text: 'Sign in'.toUpperCase()),
-                          Tab(text: 'sign up '.toUpperCase()),
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                      child: TabBarView(
-                        controller: _controller,
-                        children: const [
-                          SignInTabB(),
-                          SignUpTabB(),
-                        ],
-                      ),
-                    )
-                  ],
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Image.asset(
+                  bg10,
+                  width: width,
+                  height: height / 3.5,
+                  fit: BoxFit.fill,
                 ),
-              ),
-            ],
+                Container(
+                  width: width,
+                  height: height / 1.25,
+                  decoration: const BoxDecoration(
+                      color: grey100,
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(24))),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        margin:
+                            const EdgeInsets.only(top: 16, left: 16, right: 16),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16),
+                            color: grey200,
+                            border: Border.all(color: grey300)),
+                        child: TabBar(
+                          onTap: (index) {
+                            setState(() {
+                              _currentIndex = index;
+                            });
+                          },
+                          controller: _controller,
+                          indicator: BoxDecoration(
+                            borderRadius: BorderRadius.circular(16),
+                            color: primary,
+                          ),
+                          labelStyle: footnote(color: grey1100),
+                          unselectedLabelStyle: footnote(color: grey1100),
+                          labelColor: grey1100,
+                          unselectedLabelColor: grey1100,
+                          indicatorColor: primary,
+                          indicatorPadding: const EdgeInsets.all(2),
+                          labelPadding:
+                              const EdgeInsets.symmetric(horizontal: 0),
+                          tabs: [
+                            Tab(text: 'Sign in'.toUpperCase()),
+                            Tab(text: 'sign up '.toUpperCase()),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: TabBarView(
+                          controller: _controller,
+                          children: const [
+                            SignInTabB(),
+                            SignUpTabB(),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
