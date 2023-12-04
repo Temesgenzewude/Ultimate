d13490a03f3e679e8376f0f07824cdd6ea2de7d2
Stargazing-11 <samuelkifle199314@gmail.com>
2023-12-05 01:04:20 +0300

    add profile injection

---
+++ ultimate-app/lib/profile_injection.dart
@@ -0,0 +1,53 @@
+import 'package:flutter_ultimate/core/network/network_info.dart';
+import 'package:flutter_ultimate/features/profiles/data/datasources/profile_remote_data_source.dart';
+import 'package:flutter_ultimate/features/profiles/data/repositories%20/profile_repo_impl.dart';
+import 'package:flutter_ultimate/features/profiles/domain/repositories/profile_repository.dart';
+import 'package:flutter_ultimate/features/profiles/domain/usecases/getUserB.dart';
+import 'package:flutter_ultimate/features/profiles/domain/usecases/getUserBProfiles.dart';
+import 'package:flutter_ultimate/features/profiles/presentation/bloc/profile_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
+final sl = GetIt.instance;
+Future<void> articleInjectionInit() async {
+  // Bloc
+
+  sl.registerFactory(
+    () => ProfileBloc(getUserBProfile: sl(), getUserBProfiles: sl()),
+  );
+
+  // usecases
+  sl.registerLazySingleton(
+    () => GetUserBProfile(repository: sl()),
+  );
+
+  sl.registerLazySingleton(
+    () => GetUserBProfiles(repository: sl()),
+  );
+
+  // Repository
+  sl.registerLazySingleton<ProfileRepository>(
+    () => ProfileRepositoryImpl(
+      remoteDataSource: sl(),
+      networkInfo: sl(),
+    ),
+  );
+
+  // Data Sources
+  sl.registerLazySingleton<ProfileRemoteDataSource>(
+    () => ProfileRemoteDataSourceImpl(client: sl()),
+  );
+
+  //core
+  sl.registerLazySingleton<NetworkInfo>(
+    () => NetworkInfoImpl(sl()),
+  );
+
+  //external
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+}
