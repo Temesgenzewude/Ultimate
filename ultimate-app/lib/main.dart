3e7552dc95941a4c41ee9b520d9c7f8f8a7ceb1b
Stargazing-11 <samuelkifle199314@gmail.com>
2023-12-07 01:52:31 +0300

    intialize feed injection

--- ultimate-app/lib/main.dart
+++ ultimate-app/lib/main.dart
@@ -2,7 +2,8 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_ultimate/dependency_indjection.dart';
-import 'package:flutter_ultimate/profile_injection.dart' as injection;
+import 'package:flutter_ultimate/feed_injection.dart' as feed_injection;
+import 'package:flutter_ultimate/profile_injection.dart' as profile_injection;
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app/app.dart';
@@ -13,7 +14,8 @@ Future<void> main() async {
   final persistedSharedPrefs = await SharedPreferences.getInstance();
   initPrefManager(prefs);
   initPersistedSharePrefManager(persistedSharedPrefs);
-  await injection.profileInjectionInit();
+  await profile_injection.profileInjectionInit();
+  await feed_injection.feedInjectionInit();
 
   runApp(const MyApp());
 }
